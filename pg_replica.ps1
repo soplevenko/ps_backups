a4fcfa53e2cc2f8a60906826f622d10713e27182
soplevenko <soplevenko@gmail.com>
2016-06-27 18:08:25 +0300

    Project init

---
+++ pg_replica.ps1
@@ -0,0 +1,10 @@
+$dump_exe = """c:\Program Files\PostgreSQL\9.2.4-1.1C\bin\pg_dump.exe"""
+$restore_exe = """c:\Program Files\PostgreSQL\9.2.4-1.1C\bin\pg_restore.exe"""
+
+$replica_file = "d:\1cproc\pg_replica\pbk-b.bak"
+
+$dump_args = "-f"+$replica_file+" -Fc -b -E UTF-8 -U postgres -w pbk-b"
+$restore_args = "-dpbk-test -Fc -c -Upostgres -w "+$replica_file
+
+Start-Process $dump_exe $dump_args -Wait  -NoNewWindow
+Start-Process $restore_exe $restore_args -Wait -NoNewWindow
