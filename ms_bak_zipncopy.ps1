c892fec62bc05146c2ae23da23e633e1726f7840
soplevenko <soplevenko@gmail.com>
2016-08-31 17:43:27 +0300

    Minor fixes after work mode launch. Added comments, extra logging.

--- ms_bak_zipncopy.ps1
+++ ms_bak_zipncopy.ps1
@@ -5,8 +5,19 @@ $path_dst = "g:\mssqlbackup_copy"
 #7-zip path
 $7zpath = """c:\Program Files\7-Zip\7z.exe"""
 
+$currentdate = Get-Date
+
+#write cleanup log
+$cleanuplog = "$path_src\cleanup.log"
+Add-Content $cleanuplog "`r`n`r`nCleanup log for $currentdate"
+
 #if no access to remote folder - do nothing
-if(-not (Test-Path $path_dst)) {exit}
+if(-not (Test-Path $path_dst)) {
+    Add-Content $cleanuplog "`r`nNo access to $path_dst"
+    Add-Content $cleanuplog "`r`nCleanup finished with error"
+    Add-Content $cleanuplog "`r`n===================================================="
+    exit 1
+}
 
 #lookup for backup files with "archive" attribute, i.e., fresh ones
 foreach ($item in Get-ChildItem $path_src -Recurse -Include @("*.bak", "*.trn") | where {$_.Attributes -eq 'Archive'}){
@@ -18,14 +29,18 @@ foreach ($item in Get-ChildItem $path_src -Recurse -Include @("*.bak", "*.trn") 
 
     $args = "u -mx "+$acrcitem+" "+$item.FullName
     Start-Process $7zpath $args -Wait
+    
+    Add-Content $cleanuplog "`r`n$item 7-ziped to $acrcitem"
 }
 
+Add-Content $cleanuplog "`r`n"
+
 #move all 7-zip archives from local to remote location 
-#Start-Process "robocopy" "$path_src $path_dst *.7z /S /W:3 /R:3 /MOV /LOG+:$path_src\movelog.log" -Wait
-robocopy $path_src $path_dst *.7z /S /W:3 /R:3 /MOV /LOG+:$path_src\movelog.log
+robocopy $path_src $path_dst *.7z /S /W:3 /R:3 /MOV /NP /LOG+:$cleanuplog
+
+Add-Content $cleanuplog "`r`n"
 
 #perform cleanup, delete old backup files
-$cleanuplog = "$path_src\cleanup.log"
 
 #backup files expiration interval in days for every timelapse
 $path_expire = @{}
@@ -37,24 +52,22 @@ $path_expire["$path_src\Weekly"] = 30
 $path_expire["$path_src\Monthly"] = 60
 
 #remote folders
-$path_expire["$path_dst\Hourly"] = 21
-$path_expire["$path_dst\Daily"] = 30
+$path_expire["$path_dst\Hourly"] = 30
+$path_expire["$path_dst\Daily"] = 90
 $path_expire["$path_dst\Weekly"] = 180
 $path_expire["$path_dst\Monthly"] = 2000
-
-$currentdate = Get-Date
-
-#write cleanup log
-Add-Content $cleanuplog "`r`n`r`nCleanup log for $currentdate"
 
 #lookup through directories for expiresd files
 foreach($path in $path_expire.Keys){
     if(Test-Path $path){
         $dateexpire = $currentdate.AddDays(-$path_expire[$path])
         foreach ($item in Get-ChildItem $path -Recurse -Include @("*.bak", "*.trn", "*.7z", "*.rar") | where {$_.CreationTime -le $dateexpire}){
-            Add-Content $cleanuplog "`r`n$item"
+            Add-Content $cleanuplog "`r`nFlush file $item"
             Remove-Item $item
         }
+    }
+    else{
+        Add-Content $cleanuplog "`r`nNo access to $path"
     }
 }
 Add-Content $cleanuplog "`r`nCleanup finished"
