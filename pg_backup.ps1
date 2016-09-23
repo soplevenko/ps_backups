ce9c69ac81c7973a9b3f99ab55da08fb285e241c
soplevenko <soplevenko@gmail.com>
2016-09-23 17:18:06 +0300

    New scripst added: interact backup for PostgreSQL, auto backup for PostgreSQL, utility script for moving "weekly" files to its location pg_backup.ps1 is old and obsolete

--- pg_backup.ps1
+++ pg_backup.ps1
@@ -1,16 +1,19 @@
+#$backup_label = (Get-Date -UFormat %y%m%d_%H%M).ToString()
+#$backup_exe = """c:\Program Files\PostgreSQL\9.2.4-1.1C\bin\pg_basebackup.exe"""
 $dump_exe = """c:\Program Files\PostgreSQL\9.2.4-1.1C\bin\pg_dump.exe"""
 $7z_exe = """c:\Program Files\7-Zip\7z.exe"""
-$arc_dir = "e:\PGSQL_Backup\bckp_data"
+$arc_dir = "d:\PGSQL_Backup"
 
-$baseslist = @("pbk-b", "usp-b")
+$baseslist = @("test1", "test2")
 foreach ($base in $baseslist) {
-    $dump_args = "-f"+$arc_dir+"\"+$base+".bak -Fc -b -E UTF-8 -U postgres -w "+$base
+    $dump_args = "-f"+$arc_dir+"\"+$base+(Get-Date -UFormat %y%m%d_%H%M%S).ToString()+".bak -Fc -b -E UTF-8 -U postgres -w "+$base
     Start-Process $dump_exe $dump_args -Wait -NoNewWindow
 }
 
-$7z_args = "a "+$arc_dir+"_"+(Get-Date -UFormat %y%m%d_%H%M%S).ToString()+".7z "+$arc_dir+"\ -mx"
+#$backup_args = " -D "+$arc_dir+$backup_dir+"\ -x -w -P -h localhost -p 5432 -U postgres -l "+ $backup_label
+#$7z_args = "a "+$arc_dir+"_"+(Get-Date -UFormat %y%m%d_%H%M%S).ToString()+".7z "+$arc_dir+"\ -mx"
 
-Start-Process $7z_exe $7z_args -Wait -NoNewWindow
+#Start-Process $7z_exe $7z_args -Wait  -NoNewWindow
 
-$rem_path = $arc_dir+$backup_dir+"\*"
-Remove-Item $rem_path -Recurse
+#$rem_path = $arc_dir+$backup_dir+"\*"
+#Remove-Item $rem_path -Recurse
